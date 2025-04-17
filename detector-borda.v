e33a7d2030371ba5f03f3a44f0792c280d7bc154
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-04-17 18:03:06 -0300

    Finalizado

--- detector-borda.v
+++ detector-borda.v
@@ -2,9 +2,19 @@ module detector_borda (
     input clk,
     input rst,
     input [1:0] entrada,
-    output [1:0] detector
+    output reg [1:0] detector
 );
     
-//insira seu código aqui
+    reg [1:0] valor_anterior;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            valor_anterior <= 2'b00;
+            detector <= 2'b00;
+        end else begin 
+            detector <= (~valor_anterior) & entrada;
+            valor_anterior <= entrada;
+        end
+    end
 
 endmodule
